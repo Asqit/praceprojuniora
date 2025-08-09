7bc44e28621e28a4bc1711cf8f53a15659b22045
Ondřej Tuček <andy@Ondrej--MacBook-Air.local>
2025-08-09 20:43:17 +0200

    fix missing cron

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,7 +4,6 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
 import * as $api_listings from "./routes/api/listings.ts";
 import * as $index from "./routes/index.tsx";
 import * as $job_list from "./islands/job-list.tsx";
@@ -14,7 +13,6 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/api/joke.ts": $api_joke,
     "./routes/api/listings.ts": $api_listings,
     "./routes/index.tsx": $index,
   },
--- main.ts
+++ main.ts
@@ -9,5 +9,18 @@ import "$std/dotenv/load.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
+import { initDb } from "./lib/db/index.ts";
+import { fetchListings } from "./lib/scraping/index.ts";
+import { Listing } from "./lib/scraping/types.ts";
+
+Deno.cron("fetch job listings", { hour: { start: 9, end: 17 } }, async () => {
+  console.log('- starting CRON job "fetch job listings"');
+  const jobs = await fetchListings();
+  const kv = await initDb();
+  for (const job of jobs as Listing[]) {
+    await kv.set(["listing", job.link], job);
+  }
+  console.log('- finished CRON job "fetch job listings"');
+});
 
 await start(manifest, config);
