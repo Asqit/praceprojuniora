792ea4507f4b29b8b98d27a3e14587098a1e7f8b
Ondřej Tuček <andy@Ondrej--MacBook-Air.local>
2025-08-12 09:00:03 +0200

    adding proxy rotation

--- lib/scraping/inet/http.ts
+++ lib/scraping/inet/http.ts
@@ -1,14 +1,8 @@
-import ky from "ky";
-
 export async function createHttpClient(): Promise<Deno.HttpClient> {
-  const proxy = await (await ky.get(
-    "https://cdn.jsdelivr.net/gh/proxifly/free-proxy-list@main/proxies/protocols/https/data.txt",
-  )).text();
-
   return Deno.createHttpClient({
     proxy: {
       transport: "http",
-      url: proxy,
+      url: "http://epvaifeg-rotate:l36n27kiw7w4@p.webshare.io:80",
     },
   });
 }
