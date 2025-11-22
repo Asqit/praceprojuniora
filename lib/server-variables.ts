759b6ff6e50829b76b42eaa7e8e2b403051cba3d
Ondřej Tuček <ondrejtucek9@gmail.com>
2025-11-22 09:02:46 +0100

    fix typo

--- lib/server-variables.ts
+++ lib/server-variables.ts
@@ -1,7 +1,7 @@
 import "@std/dotenv/load";
 
 export const CONTACT_EMAIL = Deno.env.get("FRESH_PUBLIC_CONTACT_EMAIL") ??
-    "BAD_VAR";
+  "BAD_VAR";
 export const ANALYTICS_LINK = Deno.env.get("ANALYTICS_LINK") ?? "BAD_VAR";
-export const ANALYTICS_UUID = Deno.env.get("ANALYTICS_UUD") ?? "BAD_VAR";
+export const ANALYTICS_UUID = Deno.env.get("ANALYTICS_UUID") ?? "BAD_VAR";
 export const USER_AGENT = `praceprojuniora-bot/1.0 (contact: ${CONTACT_EMAIL})`;
