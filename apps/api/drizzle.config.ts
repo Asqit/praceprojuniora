d37077fbd7357ea12d48b9bcd31e9dee2de11c19
Ondřej Tuček <ondrejtucek9@gmail.com>
2026-04-20 17:34:53 +0200

    fix: invalid migrations directory

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,12 +1,12 @@
-import { config } from 'dotenv';
-import { defineConfig } from 'drizzle-kit';
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
 
-config({ path: '.env' });
+config({ path: ".env" });
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
-  out: './migrations',
-  dialect: 'turso',
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dialect: "turso",
   dbCredentials: {
     url: process.env.DB_URL!,
     authToken: process.env.DB_AUTH_TOKEN!,
